b004cd8795c269929b723d42b0553446eea441fd
gragonvlad <gragonvlad@gmail.com>
2024-02-10 20:28:12 +0300

    how add to with more

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -6,7 +6,7 @@ use App\Models\Flight;
 use App\Http\Requests\StoreFlightRequest;
 use App\Http\Requests\UpdateFlightRequest;
 use Illuminate\Support\Collection;
-
+use Carbon\Carbon;
 class FlightController extends Controller
 {
     /**
@@ -27,10 +27,14 @@ class FlightController extends Controller
      */
     public function view()
     {
+        $todayDate = Carbon::now()->format('Y-m-d');
+
+       // $ll = dd($todayDate);
         return view('compact')
             ->with('name', 'John Doe')
             ->with('age', 30)
             ->with('address', '123 Main St');
+          
     }
 public function refref()
 {
