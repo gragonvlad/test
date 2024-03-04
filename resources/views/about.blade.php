5e0639d8ffb567b248081963e064e92592c531ba
gragonvlad <gragonvlad@gmail.com>
2024-03-04 13:21:19 +0300

    typo tag

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-<?php use App\Models\BlogPost; ?>
+
 @extends('app')
 @section('content')
 
@@ -6,8 +6,6 @@
 <checkrbox :users="{{ $usersJson }}"></checkrbox>
 <ffa></ffa>
 
-</body>
-</header>
 <main>
 <h1 class="hh">Test worked</h1>
 </main>
